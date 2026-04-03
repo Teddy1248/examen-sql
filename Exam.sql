5e44958f7b7e848be81c75b588d4afbabdf174d1
Teddy Sibu <sibut@DESKTOP-IPR1CT7.localdomain>
2026-04-03 15:49:25 +0200

    Examen SQL

---
+++ Exam.sql
@@ -0,0 +1,23 @@
+1 - DESCRIBE nom_de_la_table; 
+2 - USE nom_de_la_base_de_données; 
+3 - SELECT * permet de séléctionner toutes catégories d une table et SELECT colonne permet de séléctionner uniquement la catégorie (colonne) d une table 
+4 - Je ne sais pas 
+5 - FROM 
+6 - Je n ai pas compris la question 
+7 - AND veut dire qu on cherche à faire une inscrution plus une autre instuction et OR permet de faire une instruction ou une autre instruction. En gros de trouver une donnée ou une autre donnée
+8 - Je ne sais pas. Je crois que c est qu on mets COUNT ( ) 
+9 - Je ne sais pas 
+10 - SELECT * FROM users; 
+11 - SELECT name FROM users; 
+12 - SELECT * FROM users WHERE age > 25; 
+13 - SELECT * FROM users WHERE city = "Paris"; 
+14 - SELECT * FROM users WHERE name OR firstname LIKE "Mar%"; On selectionne toutes les colonnes d une ligne et on cherche si le nom ou le prénom commence par Mar 
+15 - SELECT * FROM users WHERE city = "Marseille" OR city = "Lille" AND age >= 18 AND age <= 30; 
+16 - Je ne sais pas 
+17 - SELECT * FROM orders; 
+18 - SELECT * FROM orders WHERE amount > 100; 
+19 - SELECT name FROM client WHERE client_id = orders.client_id; 
+20 - SELECT * FROM orders WHERE client_id; 
+21 - Je ne sais pas. Je pense qu on additionne le montant de toutes les commandes. 
+22 - Je ne sais pas. Je pense qu on utilise le mot clé AVERAGE ( ). Je pense qu on additionne le montant de toutes les commandes et qu on divise ce montant par le nombre de commandes. 
+23 - Je ne sais pas 
